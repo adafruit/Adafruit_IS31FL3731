4be689d51807d0ff2f8c3e8198e9bad0562ff09f
Doxygen CI <ci-arduino@invalid>
2021-06-19 17:02:40 +0000

    Deploy docs to GitHub Pages from commit c33b11850c
    Commit: c33b11850c2a24882364fe74fee7bdec3fed87f1
    GitHub Actions run: 952813945

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___i_s31_f_l3731.html#abb2902bf5f6c86a77366682ce8c6317b',1,'Adafruit_IS31FL3731']]]
+  ['begin',['begin',['../class_adafruit___i_s31_f_l3731.html#a1afe040eadd1b6eb08dddc6d0968d52a',1,'Adafruit_IS31FL3731']]]
 ];
--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['writeregister8',['writeRegister8',['../class_adafruit___i_s31_f_l3731.html#a0ea8fa63cc0ac34692a1527552587dff',1,'Adafruit_IS31FL3731']]]
+  ['writeregister8',['writeRegister8',['../class_adafruit___i_s31_f_l3731.html#a1499e319306d7de0d5e5034a2fe7bcae',1,'Adafruit_IS31FL3731']]]
 ];
